82cec0eaff94bb6919cb91b51416d2e0299e270d
statox <adrien.fabre92@gmail.com>
2014-10-17 09:15:34 +0200

    removed: not working version

--- application/views/base/footer.php
+++
@@ -1,6 +0,0 @@
-</main> <!-- Closes the tag opened in <controller>/base.php -->
-        <footer>
-            <b>CaRaMel Prototype v8.4-dev</b> - Efrei - <em>Contact :  pa8-club-du-don@googlegroups.com</em>
-        </footer>
-	</body>
-</html>
