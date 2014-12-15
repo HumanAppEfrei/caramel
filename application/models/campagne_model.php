fb38f5a45561ae5cb2485da7c1ccef82bc146236
Neijann <niescudier@gmail.com>
2014-12-15 09:57:00 +0100

    Update campagne_model.php

--- application/models/campagne_model.php
+++ application/models/campagne_model.php
@@ -1,140 +1,226 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * Campagne_model est la classe qui definit le modele d'une campagne
- */
-class Campagne_model extends MY_Model
+* Don_model est la classe qui definit le modele d'un don
+*/
+class Don_model extends MY_Model
 {
 	/** @var (String) Nom de la table dans la BDD */
-	protected $table = 'campagnes';
-	/** @var (String) Cle primaire de cette table */
-	protected $PKey = 'CAM_ID';
+        protected $table = 'dons';
+        /** @var (String) Cle primaire de cette table */
+	protected $PKey = 'DON_ID';
 	
 	/**
-     	*  Selectionne tous les tuples de la table Campagne
-	*  @return (Mixed[]) toutes les campagnes, mixed[] signifiant plusieurs types possibles
-        **/
+	* Selectionne tous les tuples de la table "dons"
+	* @return (Mixed[]) tous les dons, mixed[] signifiant plusieurs types possibles
+	**/
 	public function select() {
 		return $this->db->select('*')->from($this->table);
 	}
 	
 	/**
-     	*  Execution de la requete et retourne le(s) resultat(s)
-	*  @return (Mixed[]) le(s) resultat(s) de la requete
-        **/
+	* Execution de la requete et retourne le(s) resultat(s)
+	* @return (Mixed[]) le(s) resultat(s) de la requete
+	**/
 	public function get_results() {
 		return $this->db->get()->result();
 	}
+    	
+    	/**
+	* Execution de la requete (determiner la reelle difference avec get_results())
+	* @return (Mixed[]) le(s) resultat(s) de la requete
+	**/
+    	public function get() {
+		return $this->db->get();
+	}
 	
 	/**
-     	*  Selectionne les campagnes en fonction d'un id
-     	*  @param (Varchar) l'id saisi
-	*  @return (Mixed[]) les campagnes avec le meme id
-        **/
+	* Selectionne les dons en fonction d'un id
+	* @param (Int) l'id saisi
+	* @return (Mixed[]) les dons avec le meme id
+	**/
 	public function read_id($id) {
-		return $this->db->where('CAM_ID', (string) $id);
+		return $this->db->where($this->table.'.'.$this->PKey, (string) $id);
 	}
 	
 	/**
-     	*  Selectionne les campagnes en fonction d'un nom
-     	*  @param (String) le nom saisi
-	*  @return (Mixed[]) les campagnes avec le meme nom (determiner l'utilisation de "after")
-        **/
-	public function read_name($name) {
-		// $sql=" CAM_NOM LIKE '%{$name}%'";
-		// return $this->db->where($sql,NULL, FALSE);
-		return $this->db->like('CAM_NOM', (string) $name, 'after');
+	* Selectionne les dons en fonction d'un montant
+	* @param (Decimal) le montant saisi
+	* @return (Mixed[]) les dons avec un montant superieur au montant saisie
+	**/
+	public function read_montant_min($min) {
+		return $this->db->where('DON_MONTANT >=', $min);
 	}
 	
 	/**
-     	*  Selectionne les campagnes en fonction d'une date
-     	*  @param (Date) la date saisie
-	*  @return (Mixed[]) les campagnes avec une date de debut posterieure a la date saisie
-        **/
-	public function read_date_debut($first_date){
-		return $this->db->where('CAM_DEBUT >=', $first_date);
+	* Selectionne les dons en fonction d'un montant
+	* @param (Decimal) le montant saisi
+	* @return (Mixed[]) les dons avec un montant inferieur au montant saisie
+	**/
+	public function read_montant_max($max) {
+		return $this->db->where('DON_MONTANT <=', $max);
 	}
 	
 	/**
-     	*  Selectionne les campagnes en fonction d'une date
-     	*  @param (Date) la date saisie
-	*  @return (Mixed[]) les campagnes avec une date de fin anterieure a la date saisie
-        **/
-	public function read_date_fin($second_date){
-		return $this->db->where('CAM_FIN <=', $second_date);
+	* Selectionne les dons en fonction d'un mode (de paiement : carte bleu, cheque, espece, virement)
+	* @param (Varchar) le mode selectionne
+	* @return (Mixed[]) les dons avec le meme mode 
+	**/
+	public function read_mode($mode) {
+		return $this->db->where('DON_MODE', (string) $mode);
 	}
 	
 	/**
-     	*  Selectionne les campagnes en fonction d'un type (fidelisation, prospection)
-     	*  @param (String) le type selectionne
-	*  @return (Mixed[]) les campagnes avec le meme type
-        **/
-	public function read_type($type){
-		return $this->db->where('CAM_TYPE', (string) $type);
+	* Selectionne les dons en fonction d'un type (de versement : carte, cheque, espece, virement)
+	* @param (Varchar) le type selectionne
+	* @return (Mixed[]) les dons avec le meme type 
+	**/
+	public function read_type($type) {
+		return $this->db->where('DON_TYPE', (string) $type);
 	}
 	
 	/**
-     	*  Selectionne les campagnes en fonction d'une des categories (web, courrier, email) avec la fonction OU
-     	*  @param (String) les categories (vide ou coche)
-	*  @return (Mixed[]) les campagnes avec au moins une categorie identique, rien quand 0 selectionnee 
-        **/
-	public function read_media_or($web, $courrier, $email){
-		$where = "(CAM_WEB LIKE '%".$web."%'
-		OR CAM_COURRIER LIKE '%".$courrier."%'
-		OR CAM_EMAIL LIKE '%".$email."%')";
-		if($web=="pasok" && $courrier=="pasok" && $email=="pasok");
-		else $this->db->where($where);
+	* Selectionne les dons en fonction d'un numero d'adherent
+	* @param (Int) le numero d'adherent
+	* @return (Mixed[]) les dons avec le meme numero
+	**/
+	public function read_numAd($numAd) {
+		return $this->db->where($this->table.'.CON_ID', (string) $numAd);
 	}
 	
-	/**
-     	*  Selectionne les campagnes en fonction d'une des categories (web, courrier, email) avec la fonction ET
-     	*  @param (String) les categories (vide ou coche)
-	*  @return (Mixed[]) les campagnes avec les memes categories
-        **/
-	public function read_media_and($web, $courrier, $email){
-		$where = "(CAM_WEB LIKE '%".$web."%'
-		AND CAM_COURRIER LIKE '%".$courrier."%'
-		AND CAM_EMAIL LIKE '%".$email."%')";
-		$this->db->where($where);
+	public function read_firstnameAd($firstnameAd) {
+		return $this->db->join('contacts as cF', 'cF.CON_ID = DONS.CON_ID')->like('cF.CON_FIRSTNAME', (string) $firstnameAd, 'after');
 	}
 	
-	/**
-     	*  Selectionne les campagnes en fonction de la categorie web
-     	*  @param (String) la categorie web (vide ou coche)
-	*  @return (Mixed[]) les campagnes avec la meme categorie
-        **/
-	public function read_web($web){
-		return $this->db->where('CAM_WEB', (string) $web);
-	}
-	
-	/**
-     	*  Selectionne les campagnes en fonction de la categorie courrier
-     	*  @param (String) la categorie courrier (vide ou coche)
-	*  @return (Mixed[]) les campagnes avec la meme categorie
-        **/
-	public function read_courrier($courrier){
-		return $this->db->where('CAM_COURRIER', (string) $courrier);
-	}
-	
-	/**
-     	*  Selectionne les campagnes en fonction de la categorie email
-     	*  @param (String) la categorie email (vide ou coche)
-	*  @return (Mixed[]) les campagnes avec la meme categorie
-        **/
-	public function read_email($email){
-		return $this->db->where('CAM_EMAIL', (string) $email);
+	public function read_lastnameAd($lastnameAd) {
+		return $this->db->join('contacts as cL', 'cL.CON_ID = DONS.CON_ID')->like('cL.CON_LASTNAME', (string) $lastnameAd, 'after');
 	}
 
-	/**
-     	*  Selectionne le montant global reçu (dons, offres) pour chaque campagne
-	*  @return (Mixed[]) les campagnes avec leur montant global reçu ainsi que leur nom et leurs dates debut/fin
-        **/
-	public function read_montant_global(){
-		return $this->db->query("SELECT C.CAM_NOM AS NOM, SUM( D.`DON_MONTANT` ) AS NUMBER , C.CAM_DEBUT AS DEBUT, C.CAM_FIN AS FIN
-			FROM  `dons` D,  `offres` O,  `campagnes` C
-				WHERE C.`CAM_ID` = O.`CAM_ID` 
-					AND O.`OFF_ID` = D.`OFF_ID` 
-						GROUP BY C.`CAM_ID` 
-							ORDER BY SUM( D.`DON_MONTANT` ) DESC");
+	public function read_Stats() {
+		return $this->db->select_avg('DON_MONTANT', 'moyenne')->select_min('DON_MONTANT', 'minimum')->select_max('DON_MONTANT', 'maximum')->select_sum('DON_MONTANT','total')->from($this->table);
+	}
+	
+	public function read_mission($mission) {
+		return $this->db->where('DON_MISSION', (string) $mission);
+	}
+	
+	public function read_inId($listId) {
+		return $this->db->select('*')->from($this->table)
+				->where_in($this->table.'.DON_ID', (array) $listId)
+				->get()->result();
+	}
+	
+	public function read_annee_creation($annee) {
+		return $this->db->like('DON_DATE', (string) $annee, 'after');
+	}
+	
+	public function read_mois_creation($mois) {
+		$mois = "-" . $mois . "-";
+		return $this->db->like('DON_DATE', (string) $mois, 'both');
+	}
+	
+	public function read_campagne($campagne) {
+		return $this->db->join('offres', 'offres.OFF_ID = DONS.OFF_ID')->where('offres.CAM_ID', (string) $campagne);
+	}
+	
+	public function update_id($old_id,$new_id) {
+		$query_data=array('CON_ID'=>$new_id);
+		$this->db->update($this->table,$query_data,"CON_ID = ".$old_id);
+	}
+
+	
+	public function read_offre($offre) {
+		return $this->db->where('dons.OFF_ID', (string) $offre);
+	}
+	
+	public function insert_flech($id,$type,$montant){
+		$data_flech=array(
+			'DON_ID'=>$id,
+			'FLECH_MONTANT'=>$montant,
+			'FLECH_TYPE' =>$type
+		);
+		//insert
+		$this->db->insert('flechages', $data_flech); 
+	}
+    
+    // Retourne les dons dont l'ID du reçu fiscal associé est $recu_id
+    public function read_recu_id($recu_id) {
+        return $this->db->where('dons.DON_RECU_ID', $recu_id);
+    }
+    
+    public function get_user_state($id, $annee){
+		// compter don de l'année de l'user
+		$sql=" SELECT SUM(DON_MONTANT) as don
+			FROM DONS 
+			WHERE CON_ID=".$id.
+			" AND YEAR(DON_DATE)=".$annee;
+			
+		$query = $this->db->query($sql);
+		
+		if($query->num_rows()>0) return $query->row();
+		else return 0;
+	}
+    
+    //limite le nombre de resultat
+    public function fetch_don($limit,$start){
+        $this->db->limit($limit,$start);
+        $query= $this->db->get();
+        
+        if ($query->num_rows()>0){
+            foreach($query->result() as $row){
+                $data[] = $row;
+            }
+            return $data;
+        }
+        return false;
+    }
+	
+	public function last_don(){
+		$sql = "SELECT MAX(DON_ID) as id
+			FROM DONS ";
+			
+		$query = $this->db->query($sql);
+		
+		if($query->num_rows()>0) return $query->row();
+		else return 0;
+	}
+
+	//requête pour le top 10 des dons, avec les noms des donateurs correspondants.
+	public function read_stat_top10_montant_avec_nom(){
+		return $this->db->select('DON_MONTANT','CON_FIRSTNAME', 'CON_LASTNAME')->join('contacts', 'contacts.CON_ID = dons.CON_ID')->order_by('DON_MONTANT', 'desc')->limit(10);
+	}
+
+	//requête pour le top 10 des donateurs, tous dons cumulés, avec leurs nom
+	public function read_stat_top10_montant_cumule(){
+		return $this->db->query("SELECT SUM(`DON_MONTANT` ) AS NUMBER , C.`CON_FIRSTNAME` , C.`CON_LASTNAME` 
+			FROM  `dons` D,  `contacts` C
+				WHERE C.`CON_ID` = D.`CON_ID` 
+					GROUP BY C.`CON_ID` 
+						ORDER BY SUM(  `DON_MONTANT` ) DESC 
+							LIMIT 0 , 10");
+	}
+
+	//requête top 10 villes donateurs
+	public function read_stat_top10_ville(){
+		return $this->db->query("SELECT  `CON_CITY` , SUM(  `DON_MONTANT` ) AS NUMBER
+			FROM  `dons` 
+				JOIN contacts ON contacts.`CON_ID` = dons.`CON_ID` 
+					GROUP BY  `CON_CITY` 
+						ORDER BY SUM(`DON_MONTANT`) DESC 
+							LIMIT 0 , 10");
+	}
+
+	//requête pour avoir la répartition des types de dons (nature, cotisation, don)
+	//query plus simple
+	public function percent_type_versement(){
+		return $this->db->query("SELECT  DON_TYPE , COUNT(  DON_TYPE ) AS NUMBER FROM  dons GROUP BY  DON_TYPE ORDER BY COUNT(  DON_TYPE ) DESC");
+	}
+
+	//requête pour avoir la répartition du mode de versement
+	public function percent_mode_versement(){
+		return $this->db->query("SELECT `DON_MODE` , COUNT(  `DON_MODE` ) AS NUMBER
+			FROM  `dons` 
+				GROUP BY `DON_MODE`
+					ORDER BY COUNT(  `DON_MODE` ) DESC");
 	}
 }
