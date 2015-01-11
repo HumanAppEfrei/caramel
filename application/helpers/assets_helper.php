881720dab7e5c93d0730d070e37eb7387e6a223c
statox <adrien.fabre92@gmail.com>
2015-01-11 12:40:51 +0100

    comments + corrected location of some files

--- application/helpers/assets_helper.php
+++ application/helpers/assets_helper.php
@@ -1,5 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/*
+ *  définition d'un ensemble de fonctions d'aide pour raccourcir l'insertion de divers éléments
+ *  grace à ces fonctions il suffit de donner le nom de l'élément à inserer et la fonction renvoit 
+ *  l'adresse complète de l'element
+ *
+ */
+
+/*
+ *  @param nom d'un fichier css
+ *  @return adresse complete du css
+ */
 if ( ! function_exists('css_url'))
 {
 	function css_url($nom)
@@ -8,6 +19,10 @@ if ( ! function_exists('css_url'))
 	}
 }
 
+/*
+ *  @param nom d'un fichier javascript
+ *  @return adresse complete du javascript
+ */
 if ( ! function_exists('js_url'))
 {
 	function js_url($nom)
@@ -16,6 +31,10 @@ if ( ! function_exists('js_url'))
 	}
 }
 
+/*
+ *  @param nom d'une image
+ *  @return adresse complete de l'image
+ */
 if ( ! function_exists('img_url'))
 {
 	function img_url($nom)
@@ -24,6 +43,10 @@ if ( ! function_exists('img_url'))
 	}
 }
 
+/*
+ *  @param nom d'une image
+ *  @return balise HTML permettant d'inserer l'image
+ */
 if ( ! function_exists('img'))
 {
 	function img($nom, $alt = '')
@@ -32,27 +55,35 @@ if ( ! function_exists('img'))
 	}
 }
 
-
+/*
+ *  @return fichier css de base
+ */
 if ( ! function_exists('bootcss_url'))
 {
 	function bootcss_url()
 	{
-		return base_url() . 'assets/bootstrap/css/bootstrap.min.css';
+		return base_url() . 'assets/css/bootstrap.min.css';
 	}
 }
 
+/*
+ *  @return fichier js de base
+ */
 if ( ! function_exists('bootjs_url'))
 {
 	function bootjs_url()
 	{
-		return base_url() . 'assets/bootstrap/js/bootstrap.min.js';
+		return base_url() . 'assets/js/bootstrap.min.js';
 	}
 }
 
+/*
+ *  @return fichier php de la bibliothèque mpdf
+ */
 if ( ! function_exists('mpdf_url'))
 {
 	function mpdf_url() {
-		return base_url . 'assets/mpdf/mpdf.php';
+		return base_url . 'application/libraries/mpdf/mpdf.php';
 	}
 }
 
