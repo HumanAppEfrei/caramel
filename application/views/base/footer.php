d83d2414fbee62e54dde878c734df55b02e41ccf
EmericBatistoni <batistoni.emeric@gmail.com>
2015-02-01 16:40:34 +0100

    integration of the third part of the documentation

--- application/views/base/footer.php
+++ application/views/base/footer.php
@@ -2,5 +2,6 @@
         <footer>
 			<a class="btn btn-large btn-success" href="javascript:void(0);" onclick="javascript:introJs().start();">Premiers pas</a>
         </footer>
+		<script>$("#aide_button").popup();</script>
 	</body>
 </html>
