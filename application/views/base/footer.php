49d36deb5b6cefe46151aee4c6a10d6d254dca6e
EmericBatistoni <batistoni.emeric@gmail.com>
2015-02-07 18:33:45 +0100

    added a summury and some cosmetic modifications

--- application/views/base/footer.php
+++ application/views/base/footer.php
@@ -2,6 +2,8 @@
         <footer>
 			<a class="btn btn-large btn-success" href="javascript:void(0);" onclick="javascript:introJs().start();">Premiers pas</a>
         </footer>
-		<script>$("#aide_button").popup();</script>
+		<script>$("#aide_button").popup({
+		width:700,height:500
+		});</script>
 	</body>
 </html>
