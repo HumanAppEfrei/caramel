4481a3a8020b3bea21624f7c13ece04c24918f59
nobe4 <haffreinguev@gmail.com>
2015-02-24 18:53:50 +0100

    added default view for all tabs : show list as in quick search

--- application/controllers/campagne.php
+++ application/controllers/campagne.php
@@ -3,24 +3,45 @@
 class Campagne extends MY_Controller {
 
 	public function index()
-	{		
+	{
 		$nav_data = array();
 		$nav_data['username'] = $this->session->userdata('username');
-		
-		$this->load->view('base/header');
-		$this->load->view('base/navigation',$nav_data);
-		$this->load->view('campagne/quicksearch');
-		$this->load->view('base/footer');
+
+		$this->load->model('campagne_model');
+		$this->load->library('form_validation');
+		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+
+			$post_selection = $this->input->post('selection');
+			$post_recherche = mysql_real_escape_string($this->input->post('recherche'));
+
+			// Vérifications des données
+
+			$items = $this->campagne_model->select();
+
+			$items = $this->campagne_model->get_results();
+
+			$list_data = array();
+			$list_data['items'] = $items;
+			$list_data['div'] = "oui";
+
+			$nav_data = array();
+			$nav_data['username'] = $this->session->userdata('username');
+
+			$this->load->view('base/header');
+			$this->load->view('base/navigation',$nav_data);
+			$this->load->view('campagne/quicksearch');
+			$this->load->view('campagne/list',$list_data);
+			$this->load->view('base/footer');
 	}
-	
-	
+
+
 	public function create()
 	{
 		$this->load->model('campagne_model');
 		$this->load->library('form_validation');
 		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
 		$post_form = $this->input->post('is_form_sent');
-		
+
 		if ($post_form)
 		{
 			// Récupération des données
@@ -34,7 +55,7 @@ class Campagne extends MY_Controller {
 			$post_web = $this->input->post('web');
 			$post_courrier = $this->input->post('courrier');
 			$post_email = $this->input->post('email');
-			
+
 			$message_debut = "";
 			$message_fin = "";
 
@@ -51,10 +72,10 @@ class Campagne extends MY_Controller {
 			if($post_fin != "--" && isValidDate(date_usfr($post_fin)) == false) $message_fin = "La date de fin saisie est incorecte";
 			$this->form_validation->set_rules('description', 'Description', 'trim|encode_php_tags|xss_clean');
 			$this->form_validation->set_rules('objectif', 'Objectif', 'trim|encode_php_tags|numeric|xss_clean');
-			
+
 			if($this->form_validation->run() && $message_debut=="" && $message_fin=="")
 			{
-			
+
 				// Envoie dans la BDD
 				$options_echappees = array();
 				$options_echappees['CAM_ID'] = $post_code;
@@ -67,41 +88,41 @@ class Campagne extends MY_Controller {
 				$options_echappees['CAM_WEB'] = $post_web;
 				$options_echappees['CAM_COURRIER'] = $post_courrier;
 				$options_echappees['CAM_EMAIL'] = $post_email;
-				
+
 				$options_non_echappees = array();
 				$options_non_echappees['CAM_DATEADDED'] = 'NOW()';
 				$options_non_echappees['CAM_DATEMODIF'] = 'NOW()';
-				
+
 				$this->campagne_model->create($options_echappees, $options_non_echappees);
-				
+
 				redirect('campagne/edit/'.$post_code, 'refresh');
-				
+
 			}
 			else
 			{
 				//	Le formulaire est invalide ou vide
 				$nav_data = array();
 				$nav_data['username'] = $this->session->userdata('username');
-				
+
 				$list_data = array();
 				$list_data['message_debut'] = $message_debut;
 				$list_data['message_fin'] = $message_fin;
-				
+
 				$this->load->view('base/header');
 				$this->load->view('base/navigation',$nav_data);
-				$this->load->view('campagne/quicksearch');			
-				$this->load->view('campagne/create',$list_data);		
+				$this->load->view('campagne/quicksearch');
+				$this->load->view('campagne/create',$list_data);
 				$this->load->view('base/footer');
 			}
-		
+
 		}
-		
+
 		else
 		{
 			// affichage
 			$nav_data = array();
 			$nav_data['username'] = $this->session->userdata('username');
-			
+
 			$this->load->view('base/header');
 			$this->load->view('base/navigation',$nav_data);
 			$this->load->view('campagne/quicksearch');
@@ -109,37 +130,37 @@ class Campagne extends MY_Controller {
 			$this->load->view('base/footer');
 		}
 	}
-	
+
 	public function quicksearch()
 	{
 		$this->load->model('campagne_model');
 		$post_form = $this->input->post('is_form_sent');
 		$this->load->library('form_validation');
 		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
-		
+
 		if ($post_form)
 		{
 			//Récupération des données
 			$post_selection = $this->input->post('selection');
 			$post_recherche = mysql_real_escape_string($this->input->post('recherche'));
-			
+
 			// Vérifications des données
-			
+
 			$items = $this->campagne_model->select();
-			
+
 			if($post_recherche!="" && $post_selection=="code") $items = $this->campagne_model->read_id($post_recherche);
 			if($post_recherche!="" && $post_selection=="nom") $items = $this->campagne_model->read_name($post_recherche);
-				
-			
+
+
 			$items = $this->campagne_model->get_results();
-	
+
 			$list_data = array();
 			$list_data['items'] = $items;
 			$list_data['div'] = "oui";
-		
+
 			$nav_data = array();
 			$nav_data['username'] = $this->session->userdata('username');
-		
+
 			$this->load->view('base/header');
 			$this->load->view('base/navigation',$nav_data);
 			$this->load->view('campagne/quicksearch');
@@ -150,15 +171,15 @@ class Campagne extends MY_Controller {
 		{
 			$this->index();
 		}
-	}	
-	
+	}
+
 	public function edit($id_campagne)
 	{
 		$this->load->model('campagne_model');
 		$this->load->library('form_validation');
 		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
 		$post_form = $this->input->post('is_form_sent');
-		
+
 		if ($post_form)
 		{
 			// Récupération des données
@@ -171,10 +192,10 @@ class Campagne extends MY_Controller {
 			$post_web = $this->input->post('web');
 			$post_courrier = $this->input->post('courrier');
 			$post_email = $this->input->post('email');
-			
+
 			$message_debut = "";
 			$message_fin = "";
-			
+
 			//vérification
 			$this->form_validation->set_rules('nom', 'Nom', 'trim|required|alpha_dash_spaces|encode_php_tags|xss_clean');
 			$this->form_validation->set_rules('jourd', 'Jour de début', 'trim|max_length[2]|numeric|encode_php_tags|xss_clean');
@@ -187,7 +208,7 @@ class Campagne extends MY_Controller {
 			if($post_fin != "--" && isValidDate(date_usfr($post_fin)) == false) $message_fin = "La date de fin saisie est incorecte";
 			$this->form_validation->set_rules('description', 'Description', 'trim|encode_php_tags|xss_clean');
 			$this->form_validation->set_rules('objectif', 'Objectif', 'trim|encode_php_tags|xss_clean');
-			
+
 			if($this->form_validation->run() && $message_debut=="" && $message_fin=="")
 			{
 				// Envoie dans la BDD
@@ -201,27 +222,27 @@ class Campagne extends MY_Controller {
 				$options_echappees['CAM_WEB'] = $post_web;
 				$options_echappees['CAM_COURRIER'] = $post_courrier;
 				$options_echappees['CAM_EMAIL'] = $post_email;
-				
+
 				$options_non_echappees = array();
 				$options_non_echappees['CAM_DATEMODIF'] = 'NOW()';
-				
+
 				$this->campagne_model->update(array('CAM_ID' => $id_campagne),$options_echappees, $options_non_echappees);
-				
+
 				redirect('campagne', 'refresh');
-				
+
 			}
 			else
 			{
 				$campagne = $this->campagne_model->read('*',array('CAM_ID' => $id_campagne));
-				
+
 				$list_data = array();
 				$list_data['campagne'] = $campagne;
 				$list_data['message_debut'] = $message_debut;
 				$list_data['message_fin'] = $message_fin;
-			
+
 				$nav_data = array();
 				$nav_data['username'] = $this->session->userdata('username');
-			
+
 				$this->load->view('base/header');
 				$this->load->view('base/navigation',$nav_data);
 				$this->load->view('campagne/quicksearch');
@@ -233,13 +254,13 @@ class Campagne extends MY_Controller {
 		else
 		{
 			$campagne = $this->campagne_model->read('*',array('CAM_ID' => $id_campagne));
-			
+
 			$list_data = array();
 			$list_data['campagne'] = $campagne;
-		
+
 			$nav_data = array();
 			$nav_data['username'] = $this->session->userdata('username');
-		
+
 			$this->load->view('base/header');
 			$this->load->view('base/navigation',$nav_data);
 			$this->load->view('campagne/quicksearch');
@@ -247,18 +268,18 @@ class Campagne extends MY_Controller {
 			$this->load->view('campagne/edit', $list_data);
 			$this->load->view('base/footer');
 		}
-	
+
 	}
-	
+
 	public function search()
 	{
 		$this->load->model('campagne_model');
 		$post_form = $this->input->post('is_form_sent');
 		$this->load->library('form_validation');
 		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
-		
+
 		if ($post_form)
-		{		
+		{
 			//Récupération des données
 			$post_code = $this->input->post('code');
 			$post_nom = $this->input->post('nom');
@@ -269,39 +290,39 @@ class Campagne extends MY_Controller {
 			$post_courrier = $this->input->post('courrier');
 			$post_email = $this->input->post('email');
 			$post_mediatype = $this->input->post('mediatype');
-			
+
 			// Vérifications des données
-			
+
 			$items = $this->campagne_model->select();
-			
+
 			if($post_code!="") $items = $this->campagne_model->read_id($post_code);
 			$this->form_validation->set_rules('code', '"Code de campagne"', 'trim|encode_php_tags|xss_clean');
-			
+
 			if($post_nom!="") $items = $this->campagne_model->read_name($post_nom);
 			$this->form_validation->set_rules('nom', '"Nom de campagne"', 'trim|encode_php_tags|xss_clean');
-			
+
 			$pieces = explode("-", $post_debut);
 			$pieces2 = explode("-", $post_fin);
 			$message = "";
-			
-			if($pieces[0]!="" && $pieces[1]!="" && $pieces[2]!="") $items = $this->campagne_model->read_date_debut($post_debut);	
+
+			if($pieces[0]!="" && $pieces[1]!="" && $pieces[2]!="") $items = $this->campagne_model->read_date_debut($post_debut);
 			else if($post_debut=="0000-00-0") $message = "Date de début incorrecte";
 			if($pieces2[0]!="" && $pieces2[1]!="" && $pieces2[2]!="") $items = $this->campagne_model->read_date_fin($post_fin);
 			else if($post_fin=="0000-00-0") $message = "Date de fin incorrecte";
-			
+
 			if($post_type!="") $items = $this->campagne_model->read_type($post_type);
-			
+
 			if ($post_mediatype=="et") $items = $this->campagne_model->read_media_and($post_web, $post_courrier, $post_email);
-			
+
 			if($post_web=="") $post_web = "pasok";
 			if($post_courrier=="") $post_courrier = "pasok";
 			if($post_email=="") $post_email = "pasok";
-			
+
 			if($post_mediatype=="ou") $items = $this->campagne_model->read_media_or($post_web, $post_courrier, $post_email);
-			
+
 			// print_r("res : ".$this->campagne_model->get_results());
 			$items = $this->campagne_model->get_results();
-			
+
 			if($this->form_validation->run() && $message=="")
 			{
 				//	Le formulaire est valide
@@ -309,15 +330,15 @@ class Campagne extends MY_Controller {
 				$list_data['items'] = $items;
 				$list_data['elements'] = $this->campagne_model->count();
 				$list_data['div'] = "oui";
-			
+
 				$nav_data = array();
 				$nav_data['username'] = $this->session->userdata('username');
-			
+
 				$this->load->view('base/header');
 				$this->load->view('base/navigation',$nav_data);
 				$this->load->view('campagne/quicksearch');
 				$this->load->view('campagne/list',$list_data);
-			
+
 				$this->load->view('base/footer');
 			}
 			else
@@ -330,7 +351,7 @@ class Campagne extends MY_Controller {
 				$search_data = array();
 				$search_data['message'] = $message;
 				$this->load->view('campagne/search',$search_data);
-	
+
 				$this->load->view('base/footer');
 			}
 		}
@@ -343,45 +364,45 @@ class Campagne extends MY_Controller {
 			$this->load->view('base/navigation',$nav_data);
 			$this->load->view('campagne/quicksearch');
 			$this->load->view('campagne/search');
-	
+
 			$this->load->view('base/footer');
 		}
 	}
-	
+
 	public function remove($id_cam)
 	{
 		// si admin
-		
+
 		$id_cam = intval($id_cam);
 		$this->load->model('campagne_model');
 		$this->load->model('offre_model');
-		
+
 		$this->offre_model->delete(array('CAM_ID' => $id_cam));
 		$this->campagne_model->delete(array('CAM_ID' => $id_cam));
 		redirect('campagne', 'refresh');
 	}
-	
+
 	public function list_offres($id_cam)
 	{
 		$this->load->model('offre_model');
 		$this->load->model('campagne_model');
-		
+
 		$items = $this->offre_model->select();
 		$items = $this->offre_model->read_camId($id_cam);
 		$items = $this->offre_model->get_results();
-		
+
 		$campagne = $this->campagne_model->select();
 		$campagne = $this->campagne_model->read_id($id_cam);
 		$campagne = $this->campagne_model->get_results();
-		
+
 		$list_data = array();
 		$list_data['items'] = $items;
 		$list_data['campagne'] = $campagne;
 		$list_data['div'] = "non";
-	
+
 		$nav_data = array();
 		$nav_data['username'] = $this->session->userdata('username');
-	
+
 		$this->load->view('base/header');
 		$this->load->view('base/navigation',$nav_data);
 		$this->load->view('campagne/quicksearch');
--- application/controllers/offre.php
+++ application/controllers/offre.php
@@ -5,27 +5,48 @@ class Offre extends MY_Controller {
 	public function index()
 	{
 		$post_form = $this->input->post('is_form_sent');
-		
+
 		$nav_data = array();
 		$nav_data['username'] = $this->session->userdata('username');
-		
-		$this->load->view('base/header');
-		$this->load->view('base/navigation',$nav_data);
-		$this->load->view('offre/quicksearch');
-		$this->load->view('base/footer');
+
+		$this->load->model('offre_model');
+		$this->load->library('form_validation');
+		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+
+			//Récupération des données
+			$post_selection = $this->input->post('selection');
+			$post_recherche = mysql_real_escape_string($this->input->post('recherche'));
+
+			// Vérifications des données
+
+			$items = $this->offre_model->select();
+			$items = $this->offre_model->get_results();
+
+			$list_data = array();
+			$list_data['items'] = $items;
+			$list_data['div'] = "oui";
+
+			$nav_data = array();
+			$nav_data['username'] = $this->session->userdata('username');
+
+			$this->load->view('base/header');
+			$this->load->view('base/navigation',$nav_data);
+			$this->load->view('offre/quicksearch');
+			$this->load->view('offre/list',$list_data);
+			$this->load->view('base/footer');
 	}
-	
+
 	public function create($id_cam = '')
 	{
 		$this->load->model('offre_model');
 		$this->load->model('campagne_model');
 		$this->load->model('segment_model');
 		$this->load->model('cible_model');
-		
+
 		$post_form = $this->input->post('is_form_sent');
 		$this->load->library('form_validation');
 		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
-		
+
 		if ($post_form)
 		{
 			//Récupération des données
@@ -37,7 +58,7 @@ class Offre extends MY_Controller {
 			$post_objectif = $this->input->post('objectif');
 			$post_campagne = $this->input->post('campagne');
 			$post_segments = $this->input->post('segments');
-			
+
 			$message_debut = "";
 			$message_fin = "";
 			$message_seg = "";
@@ -57,17 +78,17 @@ class Offre extends MY_Controller {
 			$this->form_validation->set_rules('objectif', 'Objectif', 'trim|encode_php_tags|numeric|xss_clean');
 			$this->form_validation->set_rules('campagne', 'Campagne associée', 'trim|encode_php_tags|xss_clean');
 			$this->form_validation->set_rules('segments', 'segments associés', 'trim|required|encode_php_tags|xss_clean');
-			
+
 			//vérification date dans un intervalle possible :
 			$date_camp = $this->campagne_model->read('CAM_DEBUT,CAM_FIN',array('CAM_ID'=>$post_campagne));
 			if(compare_Date($date_camp[0]->CAM_DEBUT,$post_debut) < 0) $message_debut = "L'offre ne peut pas débuter avant le début de la campagne";
 			if(compare_Date($post_debut,$post_fin) < 0) $message_debut = "La date de début ne peut pas être superieure à la date de fin";
 			if(compare_Date($post_fin,$date_camp[0]->CAM_FIN) < 0) $message_fin = "L'offre ne peut pas finir après la fin de la campagne";
-			
+
 			if($this->form_validation->run() && $message_debut=="" && $message_fin=="" && $message_seg == "")
 			{
 				$this->offre_model->begin();
-			
+
 				$options_echappees = array();
 				$options_echappees['OFF_ID'] = $post_code;
 				$options_echappees['OFF_NOM'] = $post_libelle;
@@ -77,13 +98,13 @@ class Offre extends MY_Controller {
 				$options_echappees['OFF_OBJECTIF'] = $post_objectif;
 				$options_echappees['CAM_ID'] = $post_campagne;
 				$options_echappees['SEGS_CODE'] = $post_segments;
-				
+
 				$options_non_echappees = array();
 				$options_non_echappees['OFF_DATEADDED'] = 'NOW()';
 				$options_non_echappees['OFF_DATEMODIF'] = 'NOW()';
-				
+
 				$this->offre_model->create($options_echappees, $options_non_echappees);
-				
+
 				//création de la cible de l'offre
 				$table_segments = explode(',',$post_segments);
 				$cible = $this->segment_model->createCible($table_segments);
@@ -97,23 +118,23 @@ class Offre extends MY_Controller {
 					$options_non_echappees = array();
 					$this->cible_model->create($options_echappees, $options_non_echappees);
 				}
-				
+
 				$this->offre_model->commit();
-				
+
 				//	Le formulaire est valide
 				 redirect('offre/edit/'.$post_code, 'refresh');
-				
+
 			}
 			else
 			{
 				//	Le formulaire est invalide ou vide
 				$nav_data = array();
 				$nav_data['username'] = $this->session->userdata('username');
-				
+
 				$campagne = $this->campagne_model->select();
 				$campagne = $this->campagne_model->read_id($id_cam);
 				$campagne = $this->campagne_model->get_results();
-				
+
 				$list_campagnes = $this->campagne_model->read_simple('CAM_ID,CAM_NOM');
 				$list_data = array();
 				$list_data['campagne'] = $campagne;
@@ -121,30 +142,30 @@ class Offre extends MY_Controller {
 				$list_data['message_debut'] = $message_debut;
 				$list_data['message_fin'] = $message_fin;
 				$list_data['message_seg'] = $message_seg;
-				
+
 				$this->load->view('base/header');
 				$this->load->view('base/navigation',$nav_data);
-				$this->load->view('offre/quicksearch');			
-				$this->load->view('offre/create',$list_data);		
+				$this->load->view('offre/quicksearch');
+				$this->load->view('offre/create',$list_data);
 				$this->load->view('base/footer');
 			}
 		}
-		
+
 		else
 		{
 			// affichage
 			$nav_data = array();
 			$nav_data['username'] = $this->session->userdata('username');
-			
+
 			$campagne = $this->campagne_model->select();
 			$campagne = $this->campagne_model->read_id($id_cam);
 			$campagne = $this->campagne_model->get_results();
-			
+
 			$list_campagnes = $this->campagne_model->read_simple('CAM_ID,CAM_NOM');
 			$list_data = array();
 			$list_data['campagne'] = $campagne;
 			$list_data['list_campagnes'] = $list_campagnes;
-			
+
 			$this->load->view('base/header');
 			$this->load->view('base/navigation',$nav_data);
 			$this->load->view('offre/quicksearch');
@@ -152,36 +173,36 @@ class Offre extends MY_Controller {
 			$this->load->view('base/footer');
 		}
 	}
-	
+
 	public function quicksearch()
 	{
 		$this->load->model('offre_model');
 		$post_form = $this->input->post('is_form_sent');
 		$this->load->library('form_validation');
 		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
-		
+
 		if ($post_form)
-		{		
+		{
 			//Récupération des données
 			$post_selection = $this->input->post('selection');
 			$post_recherche = mysql_real_escape_string($this->input->post('recherche'));
-			
+
 			// Vérifications des données
-			
+
 			$items = $this->offre_model->select();
-			
+
 			if($post_recherche!="" && $post_selection=="code") $items = $this->offre_model->read_id($post_recherche);
 			if($post_recherche!="" && $post_selection=="libelle") $items = $this->offre_model->read_name($post_recherche);
-			
+
 			$items = $this->offre_model->get_results();
-	
+
 			$list_data = array();
 			$list_data['items'] = $items;
 			$list_data['div'] = "oui";
-		
+
 			$nav_data = array();
 			$nav_data['username'] = $this->session->userdata('username');
-		
+
 			$this->load->view('base/header');
 			$this->load->view('base/navigation',$nav_data);
 			$this->load->view('offre/quicksearch');
@@ -193,14 +214,14 @@ class Offre extends MY_Controller {
 			$this->index();
 		}
 	}
-	
+
 	public function search()
 	{
 		$this->load->model('offre_model');
 		$post_form = $this->input->post('is_form_sent');
 		$this->load->library('form_validation');
 		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
-		
+
 		if ($post_form)
 		{
 			//Récupération des données
@@ -213,7 +234,7 @@ class Offre extends MY_Controller {
 			$post_don = $this->input->post('don');
 			$post_debut = $this->input->post('anneed')."-".$this->input->post('moisd')."-".$this->input->post('jourd');
 			$post_fin = $this->input->post('anneef')."-".$this->input->post('moisf')."-".$this->input->post('jourf');
-			
+
 			$message_debut = "";
 			$message_fin = "";
 
@@ -231,23 +252,23 @@ class Offre extends MY_Controller {
 			$this->form_validation->set_rules('description', 'Description', 'trim|encode_php_tags|xss_clean');
 			$this->form_validation->set_rules('objectif', 'Objectif', 'trim|encode_php_tags|xss_clean');
 			$this->form_validation->set_rules('campagne', 'Campagne associée', 'trim|encode_php_tags|xss_clean');
-			
+
 			// Traitement des données
 			$items = $this->offre_model->select();
 			if($post_code!="") $items = $this->offre_model->read_id($post_code);
-			if($post_nom!="") $items = $this->offre_model->read_name($post_nom);			
+			if($post_nom!="") $items = $this->offre_model->read_name($post_nom);
 			$items = $this->offre_model->get_results();
-			
+
 			if($this->form_validation->run())
 			{
 				//	Le formulaire est valide
 				$list_data = array();
 				$list_data['items'] = $items;
 				$list_data['div'] = "oui";
-			
+
 				$nav_data = array();
 				$nav_data['username'] = $this->session->userdata('username');
-			
+
 				$this->load->view('base/header');
 				$this->load->view('base/navigation',$nav_data);
 				$this->load->view('offre/quicksearch');
@@ -259,7 +280,7 @@ class Offre extends MY_Controller {
 				//	Le formulaire est invalide ou vide
 				$nav_data = array();
 				$nav_data['username'] = $this->session->userdata('username');
-				
+
 				$this->load->view('base/header');
 				$this->load->view('base/navigation',$nav_data);
 				$this->load->view('offre/quicksearch');
@@ -272,7 +293,7 @@ class Offre extends MY_Controller {
 			// affichage
 			$nav_data = array();
 			$nav_data['username'] = $this->session->userdata('username');
-			
+
 			$this->load->view('base/header');
 			$this->load->view('base/navigation',$nav_data);
 			$this->load->view('offre/quicksearch');
@@ -280,7 +301,7 @@ class Offre extends MY_Controller {
 			$this->load->view('base/footer');
 		}
 	}
-	
+
 	public function edit($id_offre)
 	{
 		$this->load->model('offre_model');
@@ -289,8 +310,8 @@ class Offre extends MY_Controller {
 		$this->load->library('form_validation');
 		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
 		$post_form = $this->input->post('is_form_sent');
-		
-		if ($post_form) 
+
+		if ($post_form)
 		{
 			$post_libelle = $this->input->post('libelle');
 			$post_debut = $this->input->post('anneed')."-".$this->input->post('moisd')."-".$this->input->post('jourd');
@@ -298,7 +319,7 @@ class Offre extends MY_Controller {
 			$post_description = $this->input->post('description');
 			$post_objectif = $this->input->post('objectif');
 			//$post_campagne = $this->input->post('campagne');
-			
+
 			$message_debut = "";
 			$message_fin = "";
 
@@ -315,7 +336,7 @@ class Offre extends MY_Controller {
 			$this->form_validation->set_rules('description', 'Description', 'trim|encode_php_tags|xss_clean');
 			$this->form_validation->set_rules('objectif', 'Objectif', 'trim|encode_php_tags|xss_clean');
 			//$this->form_validation->set_rules('campagne', 'Campagne associée', 'trim|encode_php_tags|xss_clean');
-			
+
 			if($this->form_validation->run() && $message_debut=="" && $message_fin=="")
 			{
 				$options_echappees = array();
@@ -325,35 +346,35 @@ class Offre extends MY_Controller {
 				$options_echappees['OFF_DESCRIPTION'] = $post_description;
 				$options_echappees['OFF_OBJECTIF'] = $post_objectif;
 				//$options_echappees['CAM_ID'] = $post_campagne;
-				
+
 				$options_non_echappees = array();
 				$options_non_echappees['OFF_DATEADDED'] = 'NOW()';
 				$options_non_echappees['OFF_DATEMODIF'] = 'NOW()';
-				
+
 				$this->offre_model->update(array('OFF_ID' => $id_offre),$options_echappees, $options_non_echappees);
-				
+
 				redirect('offre', 'refresh');
 			}
 			else
-			{	
+			{
 				$items = $this->offre_model->select();
 				$items = $this->offre_model->read_id($id_offre);
 				$items = $this->offre_model->get_results();
-				
+
 				$list_data = array();
 				$list_data['items'] = $items;
-				
+
 				$campagne = $this->campagne_model->read('CAM_ID,CAM_NOM',array('CAM_ID'=>$items[0]->CAM_ID));
 				$list_data['campagne'] = $campagne[0];
-				
+
 				$list_data['segments'] = explode(",",$items[0]->SEGS_CODE);
-				
+
 				$list_data['message_debut'] = $message_debut;
 				$list_data['message_fin'] = $message_fin;
-			
+
 				$nav_data = array();
 				$nav_data['username'] = $this->session->userdata('username');
-			
+
 				$this->load->view('base/header');
 				$this->load->view('base/navigation',$nav_data);
 				$this->load->view('offre/quicksearch');
@@ -366,18 +387,18 @@ class Offre extends MY_Controller {
 			$items = $this->offre_model->select();
 			$items = $this->offre_model->read_id($id_offre);
 			$items = $this->offre_model->get_results();
-			
+
 			$list_data = array();
 			$list_data['items'] = $items;
-			
+
 			$campagne = $this->campagne_model->read('CAM_ID,CAM_NOM',array('CAM_ID'=>$items[0]->CAM_ID));
 			$list_data['campagne'] = $campagne[0];
-				
+
 			$list_data['segments'] = explode(",",$items[0]->SEGS_CODE);
-		
+
 			$nav_data = array();
 			$nav_data['username'] = $this->session->userdata('username');
-		
+
 			$this->load->view('base/header');
 			$this->load->view('base/navigation',$nav_data);
 			$this->load->view('offre/quicksearch');
@@ -385,19 +406,19 @@ class Offre extends MY_Controller {
 			$this->load->view('base/footer');
 		}
 	}
-	
+
 	public function remove($id_OFF)
-	{		
+	{
 		//suppression cible de l'offre
 		$this->load->model('cible_model');
 		$this->cible_model->delete(array('OFF_ID'=>$id_OFF));
 		//suppresion offre
 		$this->load->model('offre_model');
 		$this->offre_model->delete(array('OFF_ID'=>$id_OFF));
-		
+
 		redirect('offre', 'refresh');
 	}
-	
+
 	//vérification segment existant
 	public function ajax_IsSeg($seg)
 	{
@@ -406,5 +427,5 @@ class Offre extends MY_Controller {
 		if(!$IsSeg) echo "false";
 		else echo "true";
 	}
-	
+
 }
