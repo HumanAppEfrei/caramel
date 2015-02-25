e70303f3d3c5ec48ffc9a20c90850e0661007734
statox <adrien.fabre92@gmail.com>
2015-02-25 15:26:58 +0100

    auto resize of the man popup

--- application/views/base/footer.php
+++ application/views/base/footer.php
@@ -1,11 +1,17 @@
 </main> <!-- Closes the tag opened in <controller>/base.php -->
-        <?php var_dump($this->router->uri->rsegments[1]); ?>
+        <?php
+            var_dump($this->router->uri->rsegments[1]);
+        ?>
 
 		<footer>
-			<a class="btn btn-large btn-success" href="javascript:void(0);" onclick="javascript:introJs().start();">Premiers pas</a>
+            <a class="btn btn-large btn-success" href="javascript:void(0);" onclick="javascript:introJs().start();">Premiers pas</a>
         </footer>
-		<script>$("#aide_button").popup({
-		width:700,height:500
-		});</script>
+        <script>
+
+            $("#aide_button").popup({
+                width:window.innerWidth*0.7,
+                height:window.innerHeight*0.9
+		    });
+        </script>
 	</body>
 </html>
