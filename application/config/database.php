4d8b173c33b19d248ec39d7694d278365dc7fb43
statox <adrien.fabre92@gmail.com>
2015-03-09 13:48:21 +0100

    plop

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'efrei';
 $db['default']['database'] = 'caramel';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
